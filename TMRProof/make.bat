bfe9854f857da639f823affe3ddc3dae53704c38
dburlyav <burlyaev.dmitry@gmail.com>
2017-08-16 22:55:18 +0200

    Delete TTR and TMR files

--- TMRProof/make.bat
+++
@@ -1,6 +0,0 @@
-set @coq=C:\Coq85\bin\coqc.exe
-echo TMR compilation start:
-%@coq% tmrDef
-%@coq% tmrPlugs
-%@coq% tmrMainTh
-echo TMR DONE
--- TTRProof/make.bat
+++
@@ -1,9 +0,0 @@
-set @coq=C:\Coq85\bin\coqc.exe
-echo ControlBlock compilation start:
-%@coq% ttrDef
-%@coq% ttrCMBlocks
-%@coq% ttrInv
-%@coq% ttrStep
-%@coq% ttrStepg
-%@coq% ttrMainTh
-echo TTR DONE
