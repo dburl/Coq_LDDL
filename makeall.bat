691023b88314c1ad531a1177954a1c6596fe4483
Dmitry Burlyaev <burlyaev.dmitry@gmail.com>
2017-09-02 16:58:01 +0200

    Update makeall.bat

--- makeall.bat
+++ makeall.bat
@@ -3,13 +3,7 @@ echo Sub-folders compilation:
 cd Common
 CALL make.bat 
 cd ..
-cd TMRProof
-CALL make.bat
-cd ..
 cd DTRProof
 CALL makeall.bat
-cd ..
-cd TTRProof
-CALL make.bat
 cd ..
 echo ALL DONE
